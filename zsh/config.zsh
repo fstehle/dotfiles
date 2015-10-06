892a3af5fa5851478776fa1a333c2c9daa1a3df9
Fabian Stehle <fabi@fstehle.de>
2015-10-06 21:58:56 +0200

    Share history between sessions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,11 @@
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
 setopt PROMPT_SUBST # Enable prompt expansion
-setopt SHARE_HISTORY # share history between sessions
 setopt COMBINING_CHARS # Support for German umlauts
+
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS

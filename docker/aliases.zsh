b12a2e6d9e43c01b31d3ac179e277b2f7eee7028
Fabian Stehle <fabian.stehle@jimdo.com>
2015-11-23 22:46:36 +0100

    Use DOCKER_MACHINE_DEFAULT variable for automatic docker configuration

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,2 @@
-alias docker='[ -z "$DOCKER_HOST" ] && [ -z "$DOCKER_MACHINE" ] && eval "$(docker-machine env default)"; docker'
-alias docker-machine-create-default='docker-machine create --driver virtualbox default'
+alias docker='[ -z "$DOCKER_HOST" ] && [ -z "$DOCKER_MACHINE" ] && eval "$(docker-machine env ${DOCKER_MACHINE_DEFAULT:-default})"; docker'
+alias docker-machine-create-default='docker-machine create --driver virtualbox ${DOCKER_MACHINE_DEFAULT:-default}'

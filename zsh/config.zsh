43dc2d3f3d38b34ce0928aa2eee6ca1dd954fbfc
Fabian Stehle <fabi@fstehle.de>
2015-11-24 19:51:50 +0100

    Do not incrementally add history

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,6 +6,6 @@ setopt PROMPT_SUBST # Enable prompt expansion
 setopt COMBINING_CHARS # Support for German umlauts
 
 setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt SHARE_HISTORY  # share history across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS

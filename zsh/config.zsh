d3c53e50102ae7656c4679c29fbf6438b489bc81
Fabian Stehle <fabian.stehle@jimdo.com>
2016-03-12 13:10:29 +0100

    not share zsh history across current running sessions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,6 +6,7 @@ setopt PROMPT_SUBST # Enable prompt expansion
 setopt COMBINING_CHARS # Support for German umlauts
 
 setopt APPEND_HISTORY # adds history
-setopt SHARE_HISTORY  # share history across sessions
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+unsetopt SHARE_HISTORY  # do not share history across current running sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS

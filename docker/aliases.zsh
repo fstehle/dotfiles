53a99291e9e2a136a4301d6103eca59473bc9c23
Fabian Stehle <mail@fstehle.com>
2018-01-14 19:18:45 +0100

    Remove docker aliases

--- docker/aliases.zsh
+++
@@ -1,2 +0,0 @@
-alias docker='[ -z "$DOCKER_HOST" ] && [ -z "$DOCKER_MACHINE" ] && eval "$(docker-machine env ${DOCKER_MACHINE_DEFAULT:-default})"; docker'
-alias docker-machine-create-default='docker-machine create --driver virtualbox ${DOCKER_MACHINE_DEFAULT:-default}'

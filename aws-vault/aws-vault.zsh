f2badaa3bc8c2156114db3b714055c179dc0f3a2
Fabian Stehle <mail@fstehle.com>
2021-07-02 09:38:01 +0200

    Improve aws-vault configuration

--- aws-vault/aws-vault.zsh
+++ aws-vault/aws-vault.zsh
@@ -1,3 +1,4 @@
-export AWS_VAULT_KEYCHAIN_NAME="login" # Use login keychain for aws-vault
-export AWS_SESSION_TTL=12h
-export AWS_ASSUME_ROLE_TTL=1h
+# Use login keychain for aws-vault
+export AWS_VAULT_KEYCHAIN_NAME="login"
+# Use long living sessions in aws-vault
+export AWS_SESSION_TTL="12h"

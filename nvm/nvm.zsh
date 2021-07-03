d8e7acbabce7890735c882ff726a01ed6ae9ac5a
Fabian Stehle <mail@fstehle.com>
2021-07-03 17:39:20 +0200

    Improve loading times for nvm

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,3 +1,8 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+# This loads nvm
+# --no-use will not dynamically check which node version to load
+# This must be manually added to the path, eg.
+# export PATH="${PATH}:${HOME}/.nvm/versions/node/v14.17.2/bin"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh" --no-use
+# This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"

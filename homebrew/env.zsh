7e9202926e0cb7775b97231af8900ccabdca0ae1
Fabian Stehle <mail@fstehle.com>
2021-07-03 18:00:07 +0200

    Add homebrew to path

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,2 +1,5 @@
 # Install homebew cask applications at /Applications folder
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --fontdir=/Library/Fonts"
+
+# Add homebrew to path
+eval "$(/opt/homebrew/bin/brew shellenv)"
